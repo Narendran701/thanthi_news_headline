5da29e8781b0fbcd834689673c4b04943d93f457
NareN <37191279+Narendran701@users.noreply.github.com>
2018-09-18 16:14:47 +0530

    Update news.py

--- news.py
+++ news.py
@@ -3,29 +3,39 @@ from bs4 import BeautifulSoup as bss
 from gtts import gTTS
 import os
 
-url = 'https://www.dailythanthi.com/'
+def thanthi():
+    url = 'https://www.dailythanthi.com/'
+    req = urlopen(url).read()
+    soup = bss(req, 'lxml')
+    hline = 'வணக்கம் செய்திகள் வசிப்பது உங்கள் பைதான்;'
+    ls = []
+    banner ='''
+  _____ _                 _   _     _   _   _                   
+ |_   _| |__   __ _ _ __ | |_| |__ (_) | \ | | _____      _____ 
+   | | | '_ \ / _` | '_ \| __| '_ \| | |  \| |/ _ \ \ /\ / / __|
+   | | | | | | (_| | | | | |_| | | | | | |\  |  __/\ V  V /\__ \\
+   |_| |_| |_|\__,_|_| |_|\__|_| |_|_| |_| \_|\___| \_/\_/ |___/
+                                                                
+'''
+    for tmp in soup.find_all('h4'):
+        ls.append(tmp.text.strip()+';')
 
 
-req = urlopen(url).read()
+    pp = ls[1:8]
+    nline = '\n\n'
+    one = '\n'
+    des = '='*120
+    under = '='*18
+    tab = '\t'*8
+    creat = 'Created by: நரேன் '
+    print(nline,des,one,banner,one,tab,creat,one,tab,under)
 
-soup = bss(req, 'lxml')
-ls = []
+    for val in pp:
+        print('\n',"{} {}".format('[*]', val))
 
-for tmp in soup.find_all('h4'):
-    ls.append(tmp.text.strip())
-
-
-pp = ls[1:8]
-nline = '\n\n'
-des = '='*50
-print(nline,des,'TAMIL_NEWS',des)
-for val in pp:
-    print('\n')
-    print("{} {}".format('[*]', val))
-
-os.chdir('E:')
-
-sam = ','.join(pp)
-
-spk = gTTS(sam, lang='ta')
-spk.save('tamil_news.mp3')
+    os.chdir('E:')
+    sam = ','.join(pp)
+    spk = gTTS(hline+sam, lang='ta')
+    spk.save('tamil_news.mp3')
+    
+thanthi()
